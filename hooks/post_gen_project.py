a7dad118a1ff516e957d91dc141568f7ead5d5c2
mrtolkien <gary.mialaret@gmail.com>
2023-01-15 12:17:34 +0900

    Adding git init in folder

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,6 +5,7 @@ import os
 # Setting up poetry
 os.system("poetry install")
 # Adding pre-commit
+os.system("git init")
 os.system("poetry run pre-commit install")
 # Running the default test for validation
 os.system("poetry run pytest")
